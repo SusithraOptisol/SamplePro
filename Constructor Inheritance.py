0b804d94c9a143a1d36406457e61c22b305c62ba
SusithraOptisol <89243847+SusithraOptisol@users.noreply.github.com>
2021-08-20 02:57:16 -0700

    Add files via upload

---
+++ Constructor Inheritance.py
@@ -0,0 +1,27 @@
+class Employee:
+    def __init__(self,EmpName,EmpId,Age,JoiningDate):
+        self.EmpName = EmpName
+        self.EmpId = EmpId
+        self.Age = Age
+        self.JoiningDate = JoiningDate
+        
+    
+    def getEmpDetails(self):
+        print("Employee name: ", self.EmpName)
+        print("Employee Id: ", self.EmpId)
+        print("Employee Age: ", self.Age)
+        print("Employee Joining Date: ", self.JoiningDate)
+        
+class Admin(Employee): #inherits the properties of Employee class
+
+    def __init__(self,EmpName,EmpId,Age,JoiningDate,IsAdmin):
+        super().__init__(EmpName,EmpId,Age,JoiningDate)
+        self.IsAdmin = IsAdmin
+        
+    def acessPermission(self):
+        print("Is this is a Admin user ", self.IsAdmin)
+        print("All permissions are granted")
+    
+Emp1 = Admin("susi",111,25,"may30",True)  #Admin employee is created
+Emp1.getEmpDetails()     #accessing the functions from employee class
+Emp1.acessPermission() 
