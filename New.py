554ed4b35120e4d4d2e3431095860bfbbc533687
SusithraOptisol <susithra.r@optisolbusiness.com>
2021-08-20 03:30:44 -0700

    Second time change

--- New.py
+++ New.py
@@ -1,2 +1,10 @@
 add = 5+6
 print(add)
+class Employee:
+    def __init__(self,EmpName,EmpId,Age,JoiningDate):
+        self.EmpName = EmpName
+        self.EmpId = EmpId
+        self.Age = Age
+        self.JoiningDate = JoiningDate
+
+employee = Employee("susi",123,11,"may 12")
