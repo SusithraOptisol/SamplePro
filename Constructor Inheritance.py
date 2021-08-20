b980e304182bea8d00e19b506f3cada6d5760a51
SusithraOptisol <susithra.r@optisolbusiness.com>
2021-08-20 03:21:20 -0700

    New commit

--- Constructor Inheritance.py
+++ Constructor Inheritance.py
@@ -21,7 +21,7 @@ class Admin(Employee): #inherits the properties of Employee class
     def acessPermission(self):
         print("Is this is a Admin user ", self.IsAdmin)
         print("All permissions are granted")
-    
+Emp  = Employee("Selva",123,25,"may",True)    
 Emp1 = Admin("susi",111,25,"may30",True)  #Admin employee is created
 Emp1.getEmpDetails()     #accessing the functions from employee class
 Emp1.acessPermission() 
